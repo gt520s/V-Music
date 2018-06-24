c5588ab7a5637522fce844e6c3676506392b93eb
Jason <gt520s@163.com>
2018-06-24 20:04:16 +0800

    add file

---
+++ src/components/Tab/tab.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="tab">
+  <router-link to="/recommend">
+    <span>推荐</span>
+  </router-link>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'tab'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
