c396be0be1c7485031e04d2543c223f6f4025ff2
Jason <gt520s@163.com>
2018-07-09 15:35:32 +0800

    xiugegeshi

---
+++ src/api/config.js
@@ -0,0 +1,17 @@
+/**
+ * @Created by Jason
+ * @Date: 2018/7/8 23:04
+ */
+
+export const commomParams = {
+  g_tk: 5381,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
