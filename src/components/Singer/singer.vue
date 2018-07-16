7e9c6bc7f11d010d2194a5b6d698822228b39b34
Jason <gt520s@163.com>
2018-07-16 10:30:46 +0800

    xiugegeshi

--- src/components/Singer/singer.vue
+++ src/components/Singer/singer.vue
@@ -1,13 +1,40 @@
 <template>
-<div class="box">歌手页面</div>
+<div class="singer"></div>
 </template>
 
 <script type="text/ecmascript-6">
+import {singerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+
+window.getUCGI5252209301812893 = function (res) {
+  console.log(res)
+}
 export default {
-  name: 'singer'
+  name: 'singer',
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      singerList().then(res => {
+        if (res.code === ERR_OK) {
+          console.log(res)
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position:fixed;
+    top:88px
+    bottom:0
+    width:100%
 </style>
